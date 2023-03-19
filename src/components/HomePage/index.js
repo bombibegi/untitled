cb845fc5cacfab7deb21988fa4ecfcec992b0979
Максим Конев <bomblibegizme@gmail.com>
2023-03-20 03:29:54 +0700

    добавил новый компонент, изменил чето, тут чисто комментарий

---
+++ src/components/HomePage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ThreeDivs() {
+return (
+<div>
+<div>Первый блок</div>
+<div>Второй блок</div>
+<div>Третий блок</div>
+</div>
+);
+}
+
+export default ThreeDivs;
